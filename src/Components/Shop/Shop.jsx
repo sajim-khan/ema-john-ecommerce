069afed31af6305e4766c0fb4ce901140ab1aaa2
sajim-khan <shossain1427@gmail.com>
2023-03-30 00:14:21 +0600

    added Load cart from local storage, find product

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -16,8 +16,17 @@ const Shop = () => {
 
   useEffect(() => {
     const storedCart = getShoppingCart();
-    console.log(storedCart);
-  }, []);
+    //step 1: get id
+    for (const id in storedCart) {
+      //step 2: get the product by using id
+      const addedProduct = products.find((product) => product.id === id);
+      
+      //step 3: get quantity of the product
+      const quantity = storedCart[id];
+      addedProduct.quantity = quantity
+      console.log(addedProduct);
+    }
+  }, [products]);
 
   const handelAddToCart = (product) => {
     //   console.log(cart);
