cca6e303cc7513ab1a4001338b0e5f603baa216d
sajim-khan <shossain1427@gmail.com>
2023-03-29 15:21:27 +0600

    Calculate shopping cart price, shipping and tax with toFixed

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,29 +1,36 @@
 import React from "react";
 import "./Cart.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash , faArrowRight} from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
-const Cart = ({cart}) => {
+const Cart = ({ cart }) => {
   //const cart = props.cart; // option 1
   //const {cart} = props; // option 2
-  
+
   console.log(cart);
-  
+
   let total = 0;
-  for (const product of cart){
-    total = total + product.price
+  let totalShipping = 0;
+  for (const product of cart) {
+    total = total + product.price;
+    totalShipping = totalShipping + product.shipping;
   }
-  
-  
+
+  const tax = (total * 7) / 100;
+
+  const grandTotal = total + totalShipping + tax;
+
   return (
     <div className="cart">
       <h5 className="title">Order summery</h5>
       <div className="item">
         <p className="Items">Selected Items: {cart.length}</p>
-        <p className="price">Total Price: ${ total}</p>
-        <p className="shipping-charge">Total Shipping Charge: $</p>
-        <p className="tax">Tax: $</p>
-        <h6 className="total">Grand Total: $</h6>
+        <p className="price">Total Price: ${total}</p>
+        <p className="shipping-charge">
+          Total Shipping Charge: ${totalShipping}
+        </p>
+        <p className="tax">Tax: ${tax.toFixed(2)}</p>
+        <h6 className="total">Grand Total: ${grandTotal.toFixed(2)}</h6>
       </div>
       <button className="clear-button">
         Clear Cart <FontAwesomeIcon icon={faTrash} />
