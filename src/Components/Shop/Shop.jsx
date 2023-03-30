00b73e6d2bbad2f303ba07cd049e80c89ac16857
sajim-khan <shossain1427@gmail.com>
2023-03-30 21:45:38 +0600

    added Handle quantity from storage to cart

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -40,12 +40,17 @@ const Shop = () => {
     //const newCart = [...cart, product];
     // if product does not exist in the cart then set quantity = 1
     // if exist then update quantity by 1
-    const exists = cart.find(product => product.id === product.id);
-    if(!exists){
-    product.quantity = 1;
+    const exists = cart.find(pd => pd.id === product.id);
+    if (!exists) {
+      product.quantity = 1;
       newCart = [...cart, product];
     }
-    
+    else{
+      exists.quantity = exists.quantity + 1;
+      const remaining = cart.filter(pd => pd.id !== product.id);
+      newCart = [...remaining, exists];
+    }
+
     setCart(newCart);
     addToDb(product.id);
   };
