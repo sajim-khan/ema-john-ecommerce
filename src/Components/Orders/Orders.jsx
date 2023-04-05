0f55ed6e4a8079acf81991c53ad69e0982d49622
sajim-khan <shossain1427@gmail.com>
2023-04-05 13:03:33 +0600

    Remove Item handler and Make Data consistent across UI

--- src/Components/Orders/Orders.jsx
+++ src/Components/Orders/Orders.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Cart from "../Cart/Cart";
 import { useLoaderData } from "react-router-dom";
-import './Orders.css'
+import "./Orders.css";
 import ReviewItem from "../ReviewItem/ReviewItem";
+import { removeFromDb } from "../../utilities/fakedb";
 
 const Orders = () => {
-  const cart = useLoaderData();
-  console.log(cart);
+  const savedCart = useLoaderData();
+  const [cart, setCart] = useState(savedCart);
+
+  const handleRemoveFromCart = (id) => {
+    const remaining = cart.filter((product) => product.id !== id);
+    setCart(remaining);
+    removeFromDb(id);
+  };
+
   return (
     <div className="shop-container">
       <div className="review-container">
         {cart.map((product) => (
-          <ReviewItem key={product.id} product={product}></ReviewItem>
+          <ReviewItem
+            key={product.id}
+            product={product}
+            handleRemoveFromCart={handleRemoveFromCart}
+          ></ReviewItem>
         ))}
       </div>
       <div className="cart-container">
@@ -22,4 +34,3 @@ const Orders = () => {
 };
 
 export default Orders;
-
--- src/Components/ReviewItem/ReviewItem.jsx
+++ src/Components/ReviewItem/ReviewItem.jsx
@@ -4,7 +4,7 @@ import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import "./ReviewItem.css";
 
 
-const ReviewItem = ({ product }) => {
+const ReviewItem = ({ product, handleRemoveFromCart }) => {
   const { id, img, price, name, quantity } = product;
   return (
     <div className="review-item">
@@ -18,7 +18,7 @@ const ReviewItem = ({ product }) => {
           Order Quantity: <span className="orange-text">{quantity}</span>
         </p>
       </div>
-      <button className="btn-delete">
+      <button onClick={() => handleRemoveFromCart(id)} className="btn-delete">
         <FontAwesomeIcon className="delete-icon" icon={faTrashAlt} />
       </button>
     </div>
